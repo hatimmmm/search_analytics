594b88be63ff754b9cba21fafff8e5c55eb8f79f
hatim ourrach <ourrachhatim0@gmail.com>
2025-08-07 00:35:44 +0100

    Update release process in Procfile and clean up importmap configuration

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-release: npm run build:css && rails db:migrate
+release: npm run build:css && rails assets:precompile && rails db:migrate
 web: bundle exec rails server -p $PORT -e $RAILS_ENV
 worker: bundle exec sidekiq
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,5 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-# Pin your custom application controller
-pin "controllers/application", to: "application.js"
