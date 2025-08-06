217b53abd5f1b442732ec399b9ab7aac27a88e6c
hatim ourrach <ourrachhatim0@gmail.com>
2025-08-06 20:25:04 +0100

    Refactor production database configuration and add Sidekiq web interface to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
   root "pages#home"
 
   get "analytics", to: "pages#analytics"
+  mount Sidekiq::Web => "/sidekiq"
 
   resources :searches, only: [:create]
 end
