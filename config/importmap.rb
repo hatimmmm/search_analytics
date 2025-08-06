a9fe9199ea1e041e5aeb9a4a6f1ddca51d4a3c08
hatim ourrach <ourrachhatim0@gmail.com>
2025-08-07 00:28:21 +0100

    Update importmap configuration and enhance JavaScript asset linking

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.16
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Pin your custom application controller
+pin "controllers/application", to: "application.js"
