11c8398a651ab847acb2433f40c5552a53b028af
hatim ourrach <ourrachhatim0@gmail.com>
2025-08-07 01:09:09 +0100

    Refactor code structure for improved readability and maintainability

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,1 @@
-import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "@hotwired/stimulus-loading";
-import * as ActiveStorage from "@rails/activestorage";
-import * as Turbo from "@hotwired/turbo-rails";
-import "controllers";
-
-window.Stimulus = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
-Stimulus.load(definitionsFromContext(context));
+import "./controllers";
