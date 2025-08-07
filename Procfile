533182b23940fe14aa383dfa536bdaf4f7d5780b
hatim ourrach <ourrachhatim0@gmail.com>
2025-08-07 01:00:38 +0100

    Remove release command from Procfile and update application.js imports for ActiveStorage and Turbo

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-release: npm run build:css && rails assets:precompile && rails db:migrate
 web: bundle exec rails server -p $PORT -e $RAILS_ENV
 worker: bundle exec sidekiq
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-loading";
+import * as ActiveStorage from "@rails/activestorage";
+import * as Turbo from "@hotwired/turbo-rails";
+import "controllers";
 
 window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
